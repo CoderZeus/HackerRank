1b60cde81717bae027cd0a4495df2b775e2b9010
Sharath <sharathkottalil@gmail.com>
2020-07-14 07:02:51 +1000

    q4 q5 q6 updates

---
+++ HackerRank/C30daysofCode/q4.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+     if((N>0)&&(N<101))
+        {
+           if(N%2==1)
+            cout<<"Weird";
+           else if ( (N>=2)&&(N<5) )
+            cout<<"Not Weird";
+            else if ( (N>=6)&&(N<20) )
+            cout<<"Weird";
+             else if ( (N>20))
+            cout<<"Not Weird";
+            
+
+        }
+        else
+          cout<<"Weird"; 
+    return 0;
+}
